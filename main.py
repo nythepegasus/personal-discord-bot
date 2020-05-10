85759c46b21f5fe178891847e5952e84563fb9e8
Nythepegasus <nicholas.yoder@aol.com>
2020-05-09 23:37:07 -0400

    Fixed wording

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ async def phrase_counts(ctx):
 async def stats(ctx):
     stat_emb = discord.Embed(title="Discord Bot's Server Stats", colour=0x00adff)
     stat_emb.add_field(name="Current Uptime", value=time.strftime("%H:%M:%S", time.gmtime(int(float(open('/proc/uptime').readline().split()[0])))))
-    stat_emb.add_field(name="RAM Usage", value=psutil.virtual_memory()[2])
+    stat_emb.add_field(name="RAM Percentage", value=psutil.virtual_memory()[2])
     stat_emb.add_field(name="CPU Percentage", value=psutil.cpu_percent())
     await ctx.send(embed=stat_emb)
 
