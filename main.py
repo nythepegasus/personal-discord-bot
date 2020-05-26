f2e5adfaef25a2179c597b1ab957b19d0b501ebd
Nythepegasus <nicholas.yoder@aol.com>
2020-05-26 13:00:33 -0400

    Blegh

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ async def beg(ctx):
     for i in data["timeouts"]:
         if ctx.message.author.id == i["person"]:
             cur_timeout = time.gmtime(time.time() - i["timeout"])
-            if cur_timeout.tm_hour < 24 and cur_timeout.tm_yday == 1 and cur_timeout.tm_year > 1970:
+            if cur_timeout.tm_hour < 24 and cur_timeout.tm_yday == 1 and cur_timeout.tm_year <= 1970:
                 await ctx.send(f'Dumbledore\'s cock has had enough of your mouth. Please wait {time.gmtime(time.time() - i["timeout"]).tm_hour} hours and {time.gmtime(time.time() - i["timeout"]).tm_min} minutes.')
                 return
     person_timeout = {
