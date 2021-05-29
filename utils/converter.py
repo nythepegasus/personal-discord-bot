ed03f9eb4192ca19f7389aa7264242d8d39ad6e2
Nythepegasus <nythepegasus84@gmail.com>
2021-05-29 07:48:35 -0400

    The IDE really messed up this one, huh?

--- utils/converter.py
+++ utils/converter.py
@@ -6,16 +6,16 @@ from schema import *
 conf_data = json.load(open("conf_files/conf.json"))
 mongoconf = conf_data['mongodb']
 mongoengine.connect(host=f"mongodb://{mongoconf['user']}:{mongoconf['password']}@"
-                         f"{mongoconf['ipport']}/{mongoconf['db']}?authSource=admin", )
+                         f"{mongoconf['ipport']}/{mongoconf['db']}?authSource=admin")
 print("Connected to MongoDB!")
 
-rand_phrases = json.load(open("db_files/random_texts.json"))['phrases'])
+rand_phrases = json.load(open("db_files/random_texts.json"))['phrases']
 
 print(f"Converting {len(rand_phrases)} random phrases..")
 
-for rphrase in rand_phrases['phrases']:
+for rphrase in rand_phrases:
     RandomText(type=rphrase['type'], points=rphrase['points'], text=rphrase['text'], author=rphrase['author'],
-queue = False).save()
+               queue = False).save()
 
 print(f"Converting done!")
 
@@ -27,14 +27,14 @@ dummy = Player(dis_id="dummy", name="dummy", house="Slytherin", items=[]).save()
 
 for phrase in phrases:
     p = Phrase(phrase=phrase['phrase'], adder="The OG Gang").save()
-PhraseCount(phrase=p, said=dummy, times=phrase['times_said']).save()
+    PhraseCount(phrase=p, said=dummy, times=phrase['times_said']).save()
 
 players = list(pathlib.Path("db_files/players/").glob("**/*"))
 
 for player in players:
     pdata = json.load(player.open())
-p = Player(dis_id=player.stem, name=pdata['name'], house=pdata['house'], items=pdata['items']).save()
-for points in pdata['points_earned']:
-    Points(player=p, house=p.house, type="transfer", points=points, season=2).save()
-for points in pdata['season_history'][0]:
-    Points(player=p, house=p.house, type="transfer", points=points, season=1).save()
+    p = Player(dis_id=player.stem, name=pdata['name'], house=pdata['house'], items=pdata['items']).save()
+    for points in pdata['points_earned']:
+        Points(player=p, house=p.house, type="transfer", points=points, season=2).save()
+    for points in pdata['season_history'][0]:
+        Points(player=p, house=p.house, type="transfer", points=points, season=1).save()
